0cc013925e2d079dec5cd0e14a202bbbc5e25dc5
Zubair Bester <Zubair.bester120390@gmail.com>
2021-10-25 17:27:57 +0200

    Completed Game

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  // Your web app's Firebase configuration
+ firebase  :{
+  apiKey: "AIzaSyBb5OUpA88WVtTQf-Oc8VRzn0kRFLCz4zU",
+  authDomain: "memorycardgame-ccfcc.firebaseapp.com",
+  projectId: "memorycardgame-ccfcc",
+  storageBucket: "memorycardgame-ccfcc.appspot.com",
+  messagingSenderId: "886697151134",
+  appId: "1:886697151134:web:7b20b4ca1af8f6cbd9c98f"
+ }
 };
+
+
